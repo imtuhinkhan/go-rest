75b5e884ee4d6280e33147bbe2ce94b459a227f4
Tuhin Khan <tuhin.khan@managedcoder.com>
2022-05-16 13:13:38 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+type book struct {
+	ID       string  `json:"id"`
+	Title    string  `json:"title"`
+	Author   string  `json:"author"`
+	Quantity int     `json:"quantity"`
+	Price    float32 `json:"price"`
+}
